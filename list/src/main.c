2a5a63092f1b20bf2f2221a43907e5a9ecacb768
ksharlen <miily@list.ru>
2020-02-16 13:38:01 +0300

    last commit from notebook

--- list/src/main.c
+++ list/src/main.c
@@ -5,10 +5,10 @@ int		main(void)
 	lines_t	lines = INIT_LINES;
 
 	input(&lines);
-	printf("old lines:\n");
+	printf("Soutce Lines:\n");
 	output(&lines);
 	parser(&lines);
-	printf("%snewList:%s\n", GREEN_COLOR, DFLT_COLOR);
+	printf("%sNew Lines:%s\n", GREEN_COLOR, DFLT_COLOR);
 	output(&lines);
 	deleteLines(&lines);
 	return (0);
